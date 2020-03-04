f372b36811e03f22fd60d5d0dad3e4a29b6d1963
ermi14 <ermikasa@gmail.com>
2020-03-04 09:42:14 +0300

    first Commit, Project structure created

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'You have pushed the button this many times:',
+        ),
+      ),
+    );
+  }
+}
